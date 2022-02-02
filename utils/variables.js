402eac090cbb8daed7ac3049d7e009ce25dec3b8
examenSeani <98610253+examenSeani@users.noreply.github.com>
2022-02-02 13:30:29 -0600

    Update variables.js
    update e1

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,7 @@
 // WARN: This is a generated file. Do not modify!
     export * from './constante'
     export const collection = 'e1-2022';
+
     //ya no es nesesario modificar las credenciales, en su lugar crear un ".env" y meter toda la info
     //poner fecha en cuando se aplicara
     // export var fechaActual = new Date(Date.now());
