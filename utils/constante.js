961794e29833423bee898796e71b141b9e0ac19f
examenSeani <98610253+examenSeani@users.noreply.github.com>
2022-02-02 10:57:31 -0600

    Update constante.js
    actualización de prueba, periodo del examen

--- utils/constante.js
+++ utils/constante.js
@@ -1,2 +1,2 @@
-export const fechaAplicacion = new Date("2022-02-02T10:06");
-export const fechaFinExamen = new Date("2022-02-02T10:07");
+export const fechaAplicacion = new Date("2022-02-02T11:00");
+export const fechaFinExamen = new Date("2022-02-02T12:00");
