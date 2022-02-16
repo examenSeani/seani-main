4ecb98f99d5166b23d124200bbbb056626161ab3
examenSeani <98610253+examenSeani@users.noreply.github.com>
2022-02-16 10:45:20 -0600

    Update constante.js
    Cambio para prueba piloto del viernes 18 de febrero de 2022

--- utils/constante.js
+++ utils/constante.js
@@ -1,2 +1,2 @@
-export const fechaAplicacion = new Date("2022-02-11T14:40");
-export const fechaFinExamen = new Date("2022-02-11T15:00");
+export const fechaAplicacion = new Date("2022-02-18T11:00");
+export const fechaFinExamen = new Date("2022-02-18T12:00");
