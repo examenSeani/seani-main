efd442e8bd3ec4e102b2ae27f32d054c558c2b3e
examenSeani <98610253+examenSeani@users.noreply.github.com>
2022-02-24 09:03:17 -0600

    Update constante.js
    Actualizamos fecha, para 1ra etapa 28 de Febrero 2022

--- utils/constante.js
+++ utils/constante.js
@@ -1,2 +1,2 @@
-export const fechaAplicacion = new Date("2022-02-18T12:00");
-export const fechaFinExamen = new Date("2022-02-18T14:00");
+export const fechaAplicacion = new Date("2022-02-28T09:00");
+export const fechaFinExamen = new Date("2022-02-28T12:30");
