74744a924d4e68b0664c3f68312a3d5c829f35d7
Víctor Tamayo <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-06-01 22:58:10 -0500

    e2-2022

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 // WARN: This is a generated file. Do not modify!
 export * from './constante'
-export const collection = 'e1-2022';
+export const collection = 'e2-2022';
 //ya no es nesesario modificar las credenciales, en su lugar crear un ".env" y meter toda la info
 //poner fecha en cuando se aplicara
 export const fechaActual = new Date(Date.now());
