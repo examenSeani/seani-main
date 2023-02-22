c82bf8b325b37bb52f8ffe005bf7325f537c2e5b
Francisco López <39685222+franlop24@users.noreply.github.com>
2023-02-22 14:42:02 -0600

    Nueva fecha - Marzo 2023

--- utils/constante.js
+++ utils/constante.js
@@ -1,2 +1,2 @@
-export const fechaAplicacion = new Date("2022-02-24T12:00");
-export const fechaFinExamen = new Date("2022-08-28T14:00");
+export const fechaAplicacion = new Date("2023-02-20T12:00");
+export const fechaFinExamen = new Date("2023-03-10T14:00");
